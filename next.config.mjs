303ca67a29db7e2a05230640d62e7ec60f9a1226
HP <lolbited@gmail.com>
2025-02-10 01:29:33 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,14 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
+  distDir : "dist",
   images: {
     unoptimized: true, // Disable default image optimization
   },
   assetPrefix: isProd ? '/Electro-Web/' : '',
   basePath: isProd ? '/Electro-Web' : '',
   output: 'export'
+
 };
 
 export default nextConfig;
